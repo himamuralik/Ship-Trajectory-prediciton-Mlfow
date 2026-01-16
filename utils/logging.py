bce90317954d6f537a30a3955de16aafc47936ec
himamuralik <84929346+himamuralik@users.noreply.github.com>
2026-01-16 14:41:15 +0530

    Delete utils directory

--- utils/logging.py
+++
@@ -1,9 +0,0 @@
-python
-import mlflow
-import mlflow.pytorch
-
-def log_experiment(params, metrics, model):
-    mlflow.log_params(params)
-    mlflow.log_metrics(metrics)
-    mlflow.pytorch.log_model(model, "model")
-
