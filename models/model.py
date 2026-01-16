16c07d6f6e2ce3371c42ee345620d804aa6351ed
himamuralik <84929346+himamuralik@users.noreply.github.com>
2026-01-16 13:48:41 +0530

    Create model.py

---
+++ models/model.py
@@ -0,0 +1,12 @@
+python
+import torch.nn as nn
+
+class TrajectoryModel(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        _, (h_n, _) = self.lstm(x)
+        return self.fc(h_n[-1])
