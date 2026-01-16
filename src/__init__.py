64d57a1f90781e7074e7837671b24f7879924d16
himamuralik <84929346+himamuralik@users.noreply.github.com>
2026-01-16 14:31:28 +0530

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+# src/__init__.py
+# This file makes 'src' a Python package
+# You can leave it completely empty, or add version info / exports
+
+__version__ = "0.1.0"
+
+# Optional: make frequently used items available directly
+# from src import main, evaluate
+# from src.models import get_model_class
